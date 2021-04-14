e6becfa4b5727c4633401e75730ec3842e117435
park.suhyuk <park.suhyuk@gmail.com>
2021-04-14 19:59:35 +0900

    [변환] HTML 파일변환

--- win_notebook_to_html.sh
+++ win_notebook_to_html.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm spark/notebooks/*.html
-docker exec -it notebook jupyter nbconvert --to html work/\*.ipynb
+winpty docker exec -it notebook jupyter nbconvert --to html work/\*.ipynb
 mv spark/notebooks/*.html spark/notebooks/html
